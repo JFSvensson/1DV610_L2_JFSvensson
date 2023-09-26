26ad48e477766af5a2df2d18e424ccfa5d519186
Fredrik Svensson <fs222id@student.lnu.se>
2023-09-26 08:24:22 +0200

    Add test for more elements

--- tests/importOrbitalElements.test.js
+++ tests/importOrbitalElements.test.js
@@ -1,7 +1,25 @@
 import importOrbitalElements from "../lib/importOrbitalElements"
 
-test('Import orbital elements for the Sun', () => {
+test('Import orbital elements for the Sun - M', () => {
   let importSunOrbitalElements = new importOrbitalElements('sun')
   let sunOrbitalElements = importSunOrbitalElements.importOrbitalElements()
   expect(sunOrbitalElements.M.constant).toBe(356.0470)
+})
+
+test('Import orbital elements for the Sun - a', () => {
+  let importSunOrbitalElements = new importOrbitalElements('sun')
+  let sunOrbitalElements = importSunOrbitalElements.importOrbitalElements()
+  expect(sunOrbitalElements.a).toBe(1.000000)
+})
+
+test('Import orbital elements for the Moon - a', () => {
+  let importMoonOrbitalElements = new importOrbitalElements('moon')
+  let moonOrbitalElements = importMoonOrbitalElements.importOrbitalElements()
+  expect(moonOrbitalElements.a).toBe(60.2666)
+})
+
+test('Import orbital elements for Mercury - a', () => {
+  let importMercuryOrbitalElements = new importOrbitalElements('mercury')
+  let mercuryOrbitalElements = importMercuryOrbitalElements.importOrbitalElements()
+  expect(mercuryOrbitalElements.a).toBe(0.387098)
 })
