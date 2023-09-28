357416e3e000a73ef2de3d24489a9ced16fbae60
Fredrik Svensson <fs222id@student.lnu.se>
2023-09-28 09:23:39 +0200

    Update class name to capital letter

--- tests/importOrbitalElements.test.js
+++ tests/importOrbitalElements.test.js
@@ -1,25 +1,25 @@
-import importOrbitalElements from "../lib/importOrbitalElements"
+import ImportOrbitalElements from "../lib/ImportOrbitalElements"
 
 test('Import orbital elements for the Sun - M', () => {
-  let importSunOrbitalElements = new importOrbitalElements('sun')
+  let importSunOrbitalElements = new ImportOrbitalElements('sun')
   let sunOrbitalElements = importSunOrbitalElements.importOrbitalElements()
   expect(sunOrbitalElements.M.constant).toBe(356.0470)
 })
 
 test('Import orbital elements for the Sun - a', () => {
-  let importSunOrbitalElements = new importOrbitalElements('sun')
+  let importSunOrbitalElements = new ImportOrbitalElements('sun')
   let sunOrbitalElements = importSunOrbitalElements.importOrbitalElements()
   expect(sunOrbitalElements.a).toBe(1.000000)
 })
 
 test('Import orbital elements for the Moon - a', () => {
-  let importMoonOrbitalElements = new importOrbitalElements('moon')
+  let importMoonOrbitalElements = new ImportOrbitalElements('moon')
   let moonOrbitalElements = importMoonOrbitalElements.importOrbitalElements()
   expect(moonOrbitalElements.a).toBe(60.2666)
 })
 
 test('Import orbital elements for Mercury - a', () => {
-  let importMercuryOrbitalElements = new importOrbitalElements('mercury')
+  let importMercuryOrbitalElements = new ImportOrbitalElements('mercury')
   let mercuryOrbitalElements = importMercuryOrbitalElements.importOrbitalElements()
   expect(mercuryOrbitalElements.a).toBe(0.387098)
 })
--- tests/whereIsTheSun.test.js
+++ tests/whereIsTheSun.test.js
@@ -1,7 +1,7 @@
-import whereIsTheSun from '../lib/whereIsTheSun'
+import WhereIsTheSun from '../lib/WhereIsTheSun'
 
 test('Calculate position of the Sun on 2023-09-10 at noon', () => {
-  let sunPosition = new whereIsTheSun('2023-09-10', 12)
+  let sunPosition = new WhereIsTheSun('2023-09-10', 12)
   let sun = sunPosition.whereIsTheSun()
   //Constants for checking the position of the Sun in an interval +- 10 minutes
   const minRightAscension = { hours: 11, minutes: 3, seconds: 0 }
